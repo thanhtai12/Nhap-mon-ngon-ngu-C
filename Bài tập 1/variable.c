b2c32713a666041c942427ac4db73ab90f13399a
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-11-12 11:09:11 +0700

    Add files via upload

---
+++ Bài tập 1/variable.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(void) {
+	//khai bao bien num kieu int;
+	int num;
+	// khai bao gia tri bien
+	num = '5';
+	// khai bao bien num ra man hinh
+    printf("bien num co gia tri:%d\n",num);
+	//khai bao bien x kieu char
+	char c;
+	//khai tao gia tri cho bien
+	c = 'a';
+	//hien thi bien c ra man hinh
+	printf("bien co gia tri: %c\n,c")
+	//hien thi dang bien num = 5 va bien c = a;
+	printf("bien num = %d va bien c = %c\n", num, c)
+    getchar();
+    return 0;
+}
