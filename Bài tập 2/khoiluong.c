9da8df67aa319cf044a683bbb7d78bdbade3a240
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-03 10:58:49 +0700

    Update khoiluong.c

--- Bài tập 2/khoiluong.c
+++ Bài tập 2/khoiluong.c
@@ -3,7 +3,9 @@
 int main(void) {
   int m;
   scanf("%d", &m);
+  //nhap m va cho gia tri//
   printf("kg    = %dg\n", m * 1000);
+  //nhap g va cho gia tri//
   printf("1000g = %dkg\n",m * 1);
   
   return 0;
