37fe2adfe2dafd6d1514757ec4ad2a1f6110eff8
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-03 10:42:40 +0700

    Add files via upload

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int m;
+  scanf("%d", &m);
+  printf("kg = %dg\n", m * 1000);
+  printf("KG  | G\n");
+  printf("%d", m);
+  printf("   |%d", m * 1000);
+  return 0;
+}
