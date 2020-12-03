bc6c8d8aac3715a53aa6ff1f34b75f0f58d74092
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-03 08:44:32 +0700

    Update hinhtron.c

--- Bài tập 1/hinhtron.c
+++ Bài tập 1/hinhtron.c
@@ -10,7 +10,10 @@ int main() {
   CV = 2 * R * PI;
   DT = R * R * PI;
   printf("R: %0.1f\n",R);
+  //R la chu vi hinh tron//
   printf("CV: %0.1f\n",CV);
+  //CV la chu vi hinh tron//
   printf("DT: %0.1f",DT);
+  //DT la dien tich hinh tron//
   return 0;
 }
