f79cbbb2c0bd9608b4bd866a3bfd386e45f65600
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-03 08:30:57 +0700

    Add files via upload

---
+++ Bài tập 1/hinhtron.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  double CV, DT, R;
+  //khai bao bien CT DT va R kieu double//
+  R = 1.5;
+  double PI = 3.14;
+  //khai bao hang ten PI kieu double//
+  //tinh chu vi dien tich hinh tron//
+  CV = 2 * R * PI;
+  DT = R * R * PI;
+  printf("CV hinh tron la: %0.1f\n",CV);
+  printf("DT hinh tron la: %0.1f",DT);
+  return 0;
+}
