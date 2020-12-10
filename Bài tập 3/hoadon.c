29c99e7fd9e6b84fec555eeeb92ea167005c60c7
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-10 11:20:20 +0700

    Add files via upload

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+  int a, b, c;
+  //khai bao gia tri//
+  printf("nhap khoi luong");
+  scanf("%d",&a);
+  printf("nhap don gia");
+  scanf("%d",&b);
+  int khoiluong, giatri, thanhtien;
+  thanhtien = a * b;
+  printf("a * b = %d",thanhtien);
+  return 0;
+
+
+}
