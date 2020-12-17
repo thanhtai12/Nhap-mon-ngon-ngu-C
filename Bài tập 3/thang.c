ece64ebc3817c9677aed814e44691bc455dfef0f
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-17 11:03:56 +0700

    Add files via upload

---
+++ Bài tập 3/thang.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void){
+  int thang;
+  printf("nhap thang");
+  scanf("%d",&thang);
+  switch (thang){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    printf("thang co 31 ngay");break;
+    case 
+    case 
+    printf("thang co 28 ngay");break;
+    case 
+    case
+    printf("thang co 30 ngay");break;
+    default
+
+
+  }
+ 
+ return 0;
+
+}
