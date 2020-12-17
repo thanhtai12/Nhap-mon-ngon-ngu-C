395cfc88c28eb3af18879c33e95c404eba71d99e
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-17 11:06:38 +0700

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -5,19 +5,22 @@ int main(void){
   scanf("%d",&thang);
   switch (thang){
     case 1:
-    case 2:
     case 3:
-    case 4:
     case 5:
-    case 6:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
     printf("thang co 31 ngay");break;
-    case 
-    case 
+    case 2:
     printf("thang co 28 ngay");break;
-    case 
-    case
+    case 4:
+    case 6:
+    case 9:
+    case 11:
     printf("thang co 30 ngay");break;
-    default
+    default :
+      printf("vui long nhap thang");
 
 
   }
