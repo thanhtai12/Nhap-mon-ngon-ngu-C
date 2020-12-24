215f0bdfbdcc25bb4d71efc58036f78906dbb7c7
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-24 10:51:30 +0700

    Update xinchao.c

--- Bài tập 3/xinchao.c
+++ Bài tập 3/xinchao.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main(void){
+  // khai bao bien dem//
   int count;
+  //hien thi dong dem//
   printf("\t this is a\n");
+  //cong thuc de dem//
   for (count=1; count<=10;count++)
+  // hien thi xin chao 10 lan//
       printf("\n \t \t xin chao");
  return 0;
 }
