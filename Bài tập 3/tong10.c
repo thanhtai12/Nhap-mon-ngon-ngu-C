4c4a0c2314b059397cd56794199f9418c0ea511f
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-24 10:50:28 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main(void){
+  // khai bao bien tong//
   int tong;
+  //cho tong bang 0//
   tong =0;
+  //khai bao bien de dem//
   int i;
+  //cong thuc dem//
   for(i=1;i<=10;i ++ )
+  // phep tinh tong//
   tong = tong + i;
+  //hien thi tong tu 1 den 10//
   printf("tong = %d\n \n",tong);
  return 0;
 
