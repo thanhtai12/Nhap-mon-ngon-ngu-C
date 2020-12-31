b9b4b2d6f4290459d8e4b538af86a1344638948d
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2020-12-31 10:09:54 +0700

    Update review_02.c

--- Bài tập 3/review_02.c
+++ Bài tập 3/review_02.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 int main() {
+ //khai bao bien a b i
  int a, b,i;
 char *str[] = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
  scanf("%d\n%d", &a, &b);
- for(int i =a; i <= b; i++) {     
+ //neu i=a va i<=b thi thuc hien vong lap
+ for(int i =a; i <= b; i++) {
+  //neu i>9 va la so chan thi dau ra la even
  if(i > 9) {
  if(i % 2 == 0)
  printf("even\n");
+  //neu i>9 va la so le thi dau ra la odd
  else printf("odd\n");
  }
  else {
+  //neu i=1 hien thi one, neu i=2 hien thi two, neu i=3 hien thi three, neu i=4 hien thi four, neu i=5 hien thi five, neui=6 hien thi six, neu i=7 hien thi seven, neu i=8 hien thi eight, neu i=9 hien thi nine
  printf("%s\n",str[i]);
  }
  }
+ //chay code
  return 0;
 }
