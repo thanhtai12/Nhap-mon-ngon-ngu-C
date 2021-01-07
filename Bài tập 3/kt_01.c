77071802e37921873455aca52521160b28e908fa
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-07 10:21:54 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,12 +1,39 @@
 #include<stdio.h>
 int main(void){
-  int a;
-  printf("bang cuu chuong");
-  for(a=1;a<10;a++){
-  printf(" \nbang nhan %d\n",a);
-  int b;
-  for(b=1;b<10;b++){
-  printf("\t%d x %d = %d\n", a, b, a*b);
-  }
-  }
+  int a,b;
+  printf("\nnbang cuu chuonh\n");
+  printf("\tbang nhan 2\n");
+  printf("2 x 1 = 2\n");
+  printf("2 x 2 = 4\n");
+  printf("2 x 3 = 6\n");
+  printf("2 x 4 = 8\n");
+  printf("2 x 5 = 10\n");
+  printf("2 x 6 = 12\n");
+  printf("2 x 7 = 14\n");
+  printf("2 x 8 = 16\n");
+  printf("2 x 9 = 18\n");
+  printf("2 x 10 = 20\n");
+  printf("\tbang nhan 3\n");
+  printf("3 x 1 =3\n");
+  printf("3 x 2 =3\n");
+  printf("3 x 3 =3\n");
+  printf("3 x 4 =3\n");
+  printf("3 x 5 =3\n");
+  printf("3 x 6 =3\n");
+  printf("3 x 7 =3\n");
+  printf("3 x 8 =3\n");
+  printf("3 x 9 =3\n");
+  printf("3 x 10 =3\n");
+  printf("\tbang nhan 4\n");
+  printf("4 x 1 = 4\n");
+  printf("4 x 2 = 8\n");
+  printf("4 x 3 = 12\n");
+  printf("4 x 4 = 16\n");
+  printf("4 x 5 = 20\n");
+  printf("4 x 6 = 24\n");
+  printf("4 x 7 = 28\n");
+  printf("4 x 8 = 32\n");
+  printf("4 x 9 = 36\n");
+  printf("4 x 10 = 40\n");
+  return 0;
 }
