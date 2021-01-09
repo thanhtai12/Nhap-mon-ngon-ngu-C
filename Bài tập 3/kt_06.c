e6326fdb46a6e8d8cf3689875b4695ead191b1d8
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-09 10:36:27 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -6,11 +6,11 @@ int main(void){
   //hang 2:3ngoi ao2khoang trang?
   //hang3:5ngoi sao 4khoang trang?
   //hang4:7ngoi sao 6 khoang trang?
-  //h=3:4hang
+  //h=3:3hang
   //hang 1:1 ngoi sao 0 khoang trang
   //hang2:2sao1khoang trang
   //hang3:2sao 1 khoang trang
-  //hang4:7 ngoi sao 6 khoang trang
+ 
   
   
   return 0;
