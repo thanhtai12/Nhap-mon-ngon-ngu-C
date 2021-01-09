d3f8973d7081d7d92316505103b0cae3260ac180
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-09 10:35:35 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -2,10 +2,15 @@
 #include<stdio.h>
 int main(void){
   //h=4:4hang
-  //hang 1: 1 ngoi sao:6 khoang trang
-  //hang 2:3ngoi ao6khoang trang?
-  //hang3:5ngoi sao 6khoang trang?
+  //hang 1: 1 ngoi sao:0khoang trang
+  //hang 2:3ngoi ao2khoang trang?
+  //hang3:5ngoi sao 4khoang trang?
   //hang4:7ngoi sao 6 khoang trang?
+  //h=3:4hang
+  //hang 1:1 ngoi sao 0 khoang trang
+  //hang2:2sao1khoang trang
+  //hang3:2sao 1 khoang trang
+  //hang4:7 ngoi sao 6 khoang trang
   
   
   return 0;
