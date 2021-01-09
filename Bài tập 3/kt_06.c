b94cb78672212943261eabc2017790f47d7f202a
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-09 10:36:50 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -7,9 +7,10 @@ int main(void){
   //hang3:5ngoi sao 4khoang trang?
   //hang4:7ngoi sao 6 khoang trang?
   //h=3:3hang
-  //hang 1:1 ngoi sao 0 khoang trang
-  //hang2:2sao1khoang trang
-  //hang3:2sao 1 khoang trang
+  //hang 1: 1 ngoi sao:0khoang trang
+  //hang 2:3ngoi ao2khoang trang?
+  //hang3:5ngoi sao 4khoang trang?
+ 
  
   
   
