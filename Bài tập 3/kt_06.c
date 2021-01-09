33faf563603e2d10ea35b682121b563687d9fad8
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-09 10:09:11 +0700

    Add files via upload

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(void){
+  //h=4:4hang
+  //hang 1: 1 ngoi sao:6 khoang trang
+  //hang 2:3ngoi ao6khoang trang?
+  //hang3:5ngoi sao 6khoang trang?
+  //hang4:7ngoi sao 6 khoang trang?
+  
+  
+  return 0;
+}
+
