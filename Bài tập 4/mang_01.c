d942031a8617706ee08ad030bf2a7dd5e3381960
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-14 08:41:32 +0700

    Add files via upload

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao mang
+  int mark[4] = {3,5,7,2};
+  //hien thi mang ra man hinh
+  printf("3,5,7,2\n");
+  //khai bao bien
+  int a;
+  //tinh tong cua mang
+  a=  3+5+7+2;
+  //hien thi tong cua mang
+  printf("tong = %d",a);
+  //chay 
+  return 0;
+}
