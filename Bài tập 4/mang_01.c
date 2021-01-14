b297e967e521155db3e7dbc2bff3f48acc74f5a5
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-14 10:31:19 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -3,12 +3,12 @@
 int main(void) {
   int i=0;
   //khai bao mang
-  int mark[4] = {3,5,7,2};
+  int a[4] = {3,5,7,2};
   for(i=0;i<4;i++){
   //hien thi mang ra man hinh
-  printf("%d\n",mark[i]);
-  //khai bao bien
+  printf("%d\n",a[i]);
   }
+  //khai bao bien tong
   int tong;
   tong= 3+5+7+2;
   //hien thi tong cua mang
