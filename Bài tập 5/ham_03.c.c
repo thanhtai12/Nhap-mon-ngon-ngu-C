becc6652fe02edd2f1f092cbf37df6d499204439
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-23 16:34:16 +0700

    Add files via upload

---
+++ Bài tập 5/ham_03.c.c
@@ -0,0 +1,33 @@
+//****************************************CODE**********
+//Khoi khai bao
+#include<stdio.h>
+void Nhap_pt_mang(int a);
+void Hien_thi_pt_mang(int a);
+void Hien_thi_tong_chan(int a);
+void Dem_chan_le_0(int a);
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+void Nhap_pt_mang(int a){
+void Hien_thi_pt_mang(int a){
+void Hien_thi_tong_chan(int a){
+void Dem_chan_le_0(int a){
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+}}}}
+//thuc hien mang
+
+// khoi dinh nghia ham
+
+//*************************END CODE*****************************
