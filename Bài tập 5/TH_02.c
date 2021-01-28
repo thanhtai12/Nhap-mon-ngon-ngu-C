48ab538e48a2ae6d772291d09d605d244d9feff0
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-28 15:17:16 +0700

    Add files via upload

---
+++ Bài tập 5/TH_02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+  int n=123;
+  int s=0;
+  printf("nhap n\t");
+  scanf("%d",&n);
+  
+    for(int i=1;n>0;i++){
+      int so=n%10;
+      n=n/10;
+      s=s+so;
+      
+    }
+   printf("s=%d",s);
+  return 0;
+}
+
