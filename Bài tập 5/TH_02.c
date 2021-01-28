947f55bda8af823ef8793e599257d79419a12a48
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-01-28 15:43:17 +0700

    Update TH_02.c

--- Bài tập 5/TH_02.c
+++ Bài tập 5/TH_02.c
@@ -1,12 +1,12 @@
   
 #include<stdio.h>
 int main(void){
-  int n=123;
+  int n;
   int s=0;
   printf("nhap n\t");
   scanf("%d",&n);
-    for(int i=1;i<n;i++){
-      s=s+i;
+    for(int a=1;a<=n;a++){
+      s=s+a;
     }
    printf("s=%d",s);
   return 0;
