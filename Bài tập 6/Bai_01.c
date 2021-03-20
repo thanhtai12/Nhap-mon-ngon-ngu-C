2d0560fdde113de2ef765e71f7f7ffc52070473b
thanhtai12 <74332737+thanhtai12@users.noreply.github.com>
2021-03-20 08:51:47 +0700

    Update Bai_01.c

--- Bài tập 6/Bai_01.c
+++ Bài tập 6/Bai_01.c
@@ -3,18 +3,15 @@
 
 int main(void) {
   int n, tong = 0;
-  printf("Nhap n = ");scanf("%d",&n);
-  for(int i = 1; i < n; i++){
+  printf("Nhap n = ");
+  scanf("%d",&n);
+  for(int i = 1; i <= n; i++){
     if(n % i == 0){
       tong = tong + i;
-      }
-    } 
-      if(tong == n){
         printf("%d la so hoan hao\n",n);
       } else {
         printf("%d la sao khong hoan hao\n",n);
       }
-    
-
+  }
   return 0;
 }
